996771f9b6b804a4d09cca2e2c250aa52efb980b
Julio Angulo <solojoas@gmail.com>
2022-06-08 22:01:46 +0000

    Continuing with terminal class

--- UI/include/terminal.h
+++ UI/include/terminal.h
@@ -1,18 +1,23 @@
 #pragma once
 
 #include "Common.h"
+#include <sys/ioctl.h>
+#include <sgtty.h>
+#include <signal.h>
 
 class Terminal {
-private:
-    //static sgttyb   ttym;   //original TTY mode
-    static const int maxRows = 240;
-    static const int maxCols = 80;
+    static sgttyb        ttym;           // original tty mode
+    static const int    maxRows = 240;
+    static const int    maxCols = 80;
+    static int          rows, cols;
+    static Terminal*    term;
 
 public:
-    //Terminal(int rows = maxRows, int cols = maxCols);
-    Terminal();
+    Terminal(int rows = maxRows, int cols = maxCols);
+    ~Terminal();
     void AlfaPen();
     void GraphPen();
+    void Error();
     void PlainPen();
 
 };
--- UI/src/terminal.cpp
+++ UI/src/terminal.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
 #include "terminal.h"
 
-Terminal::Terminal()
-{}
+Terminal* Terminal::term = nullptr;
+int Terminal::rows = maxRows;
+int Terminal::cols = maxCols;
+
+Terminal::Terminal(int rows, int cols)
+{
+    if(term)
+    {
+        //TODO: print error and kill terminal
+        //Terminal shall be called once
+    }
+
+    term = this;
+    Terminal::rows = rows = (rows <= 0 || rows > maxRows ? maxRows : rows);
+    Terminal::cols = cols = (cols <= 0 || cols > maxCols ? maxCols : cols);
+}
+
+Terminal::~Terminal()
+{
+    std::cout << "[DEBUG]: called Terminal destructor" << std::endl; 
+}
